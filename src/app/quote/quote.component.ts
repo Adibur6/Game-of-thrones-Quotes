55c38e3aba7d868c0ea9c85ce311a3c438eeb0f8
adibur6 <adibur6@gmail.com>
2023-10-02 12:35:05 +0600

    called api to get data

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
-export class QuoteComponent {
+export class QuoteComponent implements OnInit {
+  quotes: string[] = [];
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getQuotes(5); // You can set the initial number of quotes here
+  }
+
+  getQuotes(numberOfQuotes: number): void {
+    this.http.get<any[]>(`https://api.gameofthronesquotes.xyz/v1/random/${numberOfQuotes}`)
+      .subscribe((data) => {
+        this.quotes = data.map(item => item.sentence);
+      });
+  }
 }
